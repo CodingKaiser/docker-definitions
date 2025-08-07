1f752f553bd8214b1fb02836c6bf1eb440672ba7
CodingKaiser <falkonoe@gmail.com>
2025-08-07 09:29:26 +0200

    4.5.1 bulk RNA

---
+++ docker/kaist-bulk/4.5.1/install.R
@@ -0,0 +1,19 @@
+install.packages("pak", repos = file.path("https://r-lib.github.io/p/pak/stable", .Platform$pkgType, R.Version()$os, R.Version()$arch))
+
+packages <- c(
+  "tidyverse",
+  "plotly",
+  "pheatmap",
+  "patchwork",
+  "clusterProfiler",
+  "org.Hs.eg.db",
+  "Rsubread",
+  "rtracklayer",
+  "SummarizedExperiment",
+  "DESeq2",
+  "edgeR",
+  "tximport",
+  "EnhancedVolcano",
+  "WGCNA"
+)
+pak::pkg_install(packages, ask=FALSE)
